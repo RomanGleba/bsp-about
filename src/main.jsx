5916dd90ec622ba44efbf14e538d9a455d5ba525
RomanGleba <romagleba@gmail.com>
2025-09-08 17:47:14 +0300

    fix bug main.jsx path component is Error.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { RouterProvider } from 'react-router-dom';
 import { appRouter } from './appRouter';
 import './styles/global.scss';
 import './i18n.js';
-import Errors from "./components/Errors/Errors.jsx";
+import Errors from "./components/errors/Errors.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
